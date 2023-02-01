dcf9dfd40b57ce7c08f212ec6f62e7d61721f026
Walter <walter.clark24@yahoo.com>
2023-02-01 14:45:04 -0500

    commit tree 64722d55f28df02ffa3bdbf79275160604fe8348

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,8 @@
 require 'pry'
 class TicTacToe
     attr_accessor :board
-    def initialize(board =  [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-          @board = board
+    def initialize  
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     end
 
      WIN_COMBINATIONS = [
@@ -28,27 +28,53 @@ class TicTacToe
         user_input.to_i - 1
     end    
 
-    def move(index, value)
-        @board[index] = value
+    def move(index, token)
+        @board[index] = token
     end
     
     def position_taken?(index)
         @board[index] == " " ? false : true
-end
+        # @board[index] != ""
+    end
+
     def valid_move?(index)
         index.between?(0,8) && !position_taken?(index)
+        
     end
+
     def turn_count
         @board.count{|position| position!= " "}
+        # @board.count("X") + @board.count("0")
     end
     
     def current_player
-        if turn_count().even? == true
-            "X"
+        turn_count.even? ? "X": "O"       
+    end
+
+    def turn
+        #ask for input
+        puts "enter 1-9"
+        #get input
+        #gets.strip
+        #get.chomp
+        # input = gets.chomp
+        #translate input into index
+        index = input_to_index(gets)
+        #if index is valid
+        if valid_move?(index)
+            #make the move for index
+            move(index, current_player)
+            #show the board
+            display_board
+            #else
+        #recursvie case: index not valide, redo turn
         else
-            "O"
-        end
+            #turn
+        turn
     
+        
+        end
+
     end
 end
 game = TicTacToe.new
